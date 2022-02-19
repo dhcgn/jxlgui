38c28a3a656c3ef6511131ce6cf69c9c384bf165
Daniel <dhcgn@users.noreply.github.com>
2022-02-19 15:38:08 +0100

    cleanup script

--- sign_file.ps1
+++ sign_file.ps1
@@ -1,9 +1,18 @@
+param ([string] $inputfile)
+
+# Vars
+$secretKeyPathLocal = "D:\*\*\*\dhcgn-github.key.base64"
+$secretKeyHash = "C73659FD39DC0C1A4A58E2CCB517322A78CB5D8DEAAFCE8E6D1A4BBEEFD08EFC"
+$minisignDownloadUrl = 'https://github.com/jedisct1/minisign/releases/download/0.10/minisign-0.10-win64.zip'
+$publicKey = "RWS6WvbGy1Vj62jz6zVQfFIy+gcXJVK1nyGOZxpOLOIQmhTziYNk9B/g"
+
+# Secrets
 $privateKey = $env:MINISIGN_KEY
 
 if ($privateKey.Length -eq "")
 {
     Write-Host "::debug::Secret was empty"
-    $privateKey = Get-Content "D:\*\*\*\dhcgn-github.key.base64"
+    $privateKey = Get-Content $secretKeyPathLocal
 }
 
 $privateKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($privateKey))
@@ -11,21 +20,21 @@ $privateKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase6
 Set-Content -Path private_key -Value $privateKey
 Get-FileHash -Path private_key -Algorithm SHA256
 
-if ((Get-FileHash -Path private_key -Algorithm SHA256).Hash -ne "C73659FD39DC0C1A4A58E2CCB517322A78CB5D8DEAAFCE8E6D1A4BBEEFD08EFC")
+if ((Get-FileHash -Path private_key -Algorithm SHA256).Hash -ne $secretKeyHash)
 {
     Write-Host "::error::Private key is not valid"
     Exit -1
 }
 
 if (-Not (Test-Path "minisign.zip")){
-    Invoke-WebRequest -Uri 'https://github.com/jedisct1/minisign/releases/download/0.10/minisign-0.10-win64.zip' -OutFile minisign.zip
+    Invoke-WebRequest -Uri $minisignDownloadUrl -OutFile minisign.zip
 }
 
 Expand-Archive -Path minisign.zip -DestinationPath . -Force
 Set-Alias -Name minisign -Value .\minisign-win64\minisign.exe
 
 Write-Host "::debug::Sign file"
-"" | minisign -Sm .\output\jxlgui.wpf.exe -s private_key
+"" | minisign -Sm $inputfile -s private_key
 Remove-Item -Path private_key -Force
 if ($LASTEXITCODE -ne 0){
     Write-Host "::error::Could't sign file"
@@ -33,7 +42,7 @@ if ($LASTEXITCODE -ne 0){
 }
 
 Write-Host "::debug::Verify signature"
-minisign -VP RWS6WvbGy1Vj62jz6zVQfFIy+gcXJVK1nyGOZxpOLOIQmhTziYNk9B/g -m .\output\jxlgui.wpf.exe
+minisign -VP $publicKey -m $inputfile
 if ($LASTEXITCODE -ne 0){
     Write-Host "::error::Couldn't verify signature"
     Exit -1
